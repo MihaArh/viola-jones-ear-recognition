ab7456544f42859a1b1e4131147313f626d8d19f
arhmi <miha.arh@telekom.si>
2020-12-07 09:17:06 +0100

    Added method for capturing image from camera

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from time import sleep
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import cv2
 
 
-# Press the green button in the gutter to run the script.
+def capture_image():
+    cam = cv2.VideoCapture(1)
+    sleep(1)
+    s, img = cam.read()
+    cam.release()
+    if s:
+        cv2.imwrite("data/mydata/camface.jpg", img)
+        plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+
+    return cv2.imread('data/mydata/camface.jpg')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    capture_image()
