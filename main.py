89423584b2a16ee1076e4064d43309ace371c97a
arhmi <miha.arh@telekom.si>
2020-12-07 09:36:44 +0100

    Added method Viols-Jones for both ears

--- main.py
+++ main.py
@@ -16,5 +16,63 @@ def capture_image():
     return cv2.imread('data/mydata/camface.jpg')
 
 
+def save_plot(img, filename):
+    images = [img]
+
+    fig = plt.figure(figsize=(10, 10))
+    fig.suptitle("Viola-Jones ear detection", fontsize=16)
+
+    for ix, image in enumerate(images):
+        ax = plt.subplot("11" + str(ix + 1))
+        ax.imshow(image)
+
+    plt.savefig(f'data/test_computed/{filename}')
+    plt.close()
+
+
+def viola_jones(img, gray):
+    left_ear_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_mcs_leftear.xml')
+    left_ears = left_ear_cascade.detectMultiScale(gray, params["left"]["scale_step"], params["left"]["size"],
+                                                  minSize=(20, 20), maxSize=(100, 100))
+
+    for predicted_coords in left_ears:
+        (x, y, w, h) = predicted_coords
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(img, 'Left ear', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 0), 1)
+
+    right_ear_cascade = cv2.CascadeClassifier('haarcascades/haarcascade_mcs_rightear.xml')
+    right_ears = right_ear_cascade.detectMultiScale(gray, params["right"]["scale_step"],
+                                                    params["right"]["size"],
+                                                    minSize=(20, 20), maxSize=(100, 100))
+    for predicted_coords in right_ears:
+        (x, y, w, h) = predicted_coords
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv2.putText(img, 'Right ear', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 1)
+
+
+def read_image(path):
+    filename = path.split("/")[-1]
+    img = cv2.imread(path)
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return filename, img, gray
+
+
+params = {
+    "left": {
+        "scale_step": 1.005,
+        "size": 3
+    },
+    "right": {
+        "scale_step": 1.005,
+        "size": 3
+    },
+}
+
 if __name__ == '__main__':
+    path = "data/mydata/camface.jpg"
     capture_image()
+    (filename, img, gray) = read_image(path)
+    viola_jones(img, gray)
+    save_plot(img, filename)
